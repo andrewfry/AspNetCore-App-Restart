d434485fac2472f29b3ca5f1e6b9479094aad6c5
Andrew Fry <andrew.fry+git@gmail.com>
2016-11-28 06:28:05 -0500

    Add project files.

---
+++ src/TestRestart/Controllers/HomeController.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TestRestart.Controllers
+{
+    public class HomeController : Controller
+    {
+        private ITest _test;
+
+        public HomeController(ITest test)
+        {
+            _test = test;
+        }
+        
+
+        public IActionResult Restart()
+        {
+            var appManager = ApplicationManager.Load();
+            appManager.Restart();
+
+            return Content("restarted");
+        }
+
+        public IActionResult Hi()
+        {
+            return Content(_test.ID);
+        }
+
+        public IActionResult About()
+        {
+            ViewData["Message"] = "Your application description page.";
+
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+
+        public IActionResult Error()
+        {
+            return View();
+        }
+    }
+}
---
+++ src/TestRestart/Program.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
+using System.Threading;
+
+namespace TestRestart
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            try
+            {
+                var appManager = ApplicationManager.Load();
+
+                do
+                {
+                    appManager.Start();
+                } while (appManager.Restarting);
+
+
+            }
+            catch (Exception ex)
+            {
+
+            }
+        }
+    }
+
+
+    public class ApplicationManager
+    {
+
+        private static ApplicationManager _appManager;
+        private IWebHost _web;
+        private CancellationTokenSource _tokenSource;
+        private bool _running;
+        private bool _restart;
+
+        public bool Restarting => _restart;
+
+        public ApplicationManager()
+        {
+            _running = false;
+            _restart = false;
+        
+        }
+
+        public static ApplicationManager Load()
+        {
+            if (_appManager == null)
+                _appManager = new ApplicationManager();
+
+            return _appManager;
+        }
+
+        public void Start()
+        {
+            if (_running)
+                return;
+
+            if (_tokenSource != null && _tokenSource.IsCancellationRequested)
+                return;
+
+            _tokenSource = new CancellationTokenSource();
+            _tokenSource.Token.ThrowIfCancellationRequested();
+            _running = true;
+
+            _web = new WebHostBuilder()
+            .UseKestrel()
+            .UseContentRoot(Directory.GetCurrentDirectory())
+            .UseIISIntegration()
+            .UseStartup<Startup>()
+            .Build();
+
+            _web.Run(_tokenSource.Token);
+        }
+
+        public void Stop()
+        {
+            if (!_running)
+                return;
+
+            _tokenSource.Cancel();
+            _running = false;
+        }
+
+        public void Restart()
+        {
+            Stop();
+
+            _restart = true;
+            _tokenSource = null;
+        }
+    }
+
+    public class Test : ITest
+
+    {
+        string _id;
+
+        public Test()
+        {
+            _id = Guid.NewGuid().ToString();
+        }
+
+        public string ID
+        {
+            get
+            {
+                return _id;
+            }
+        }
+
+    }
+
+    public interface ITest
+    {
+        string ID { get; }
+    }
+
+}
